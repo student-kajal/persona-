1ad89fefe380167b25df9e053d0a12e22e0173d9
student-kajal <kajal971126@gmail.com>
2025-08-30 03:04:23 +0530

    Updated axios baseURL config for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,27 @@
+// import axios from 'axios';
+
+// //const base = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api' || 'https://gp-fax1.onrender.com'  // your backend server address and port
+//     //baseURL: `${base}/api`
+// });
+
+// api.interceptors.request.use(config => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     config.headers['x-auth-token'] = token;
+//   }
+//   return config;
+// });
+
+// export default api;
 import axios from 'axios';
 
-//const base = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api' || 'https://gp-fax1.onrender.com'  // your backend server address and port
-    //baseURL: `${base}/api`
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5000/api'
+      : process.env.REACT_APP_API_BASE_URL + '/api'
 });
 
 api.interceptors.request.use(config => {
